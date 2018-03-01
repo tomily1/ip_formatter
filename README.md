9e1b907a6609728fcad038bd9b21d3832d2dec5b
andela-tisrael <itomilayo@gmail.com>
2018-03-01 14:55:45 +0100

    create command line ruby app

---
+++ README.md
@@ -0,0 +1,17 @@
+ #### ``` Problem Set: Given a  an input of IPV4 addresses in a txt file, create a command line application that takes the input and prints out a set of unique ips with addresses in sorted order ```
+
+```
+Example: Given IPs 1.2.3.5:25,7,8  1.2.3.5:25,7,8,8,1  1.2.3.6:10,1,12,1,1,14,2,1,9,5,7 in a text file
+
+Output should be: 
+    1.2.3.5: 1, 7, 8, 25
+    1.2.3.6:1, 2, 5, 7, 10, 12, 14
+```
+
+#### How to Run Program
+
+1. `git clone` the repo 
+2. `cd` into the folder cloned
+3.  run `ruby ip_formatter.rb 'text_file_name.txt'` on the command line
+    3b. to use the test file included in this repository, run `ruby ip_formatter.rb './test.txt'` on the command line
+4. Tada! you have your result displayed :)
---
+++ ip_formatter.rb
@@ -0,0 +1,60 @@
+class IpFormatter
+  attr_accessor :data, :file, :result
+  
+  def initialize(file)
+    @file = file
+    @result = {}
+  end
+  
+  def prepare_file
+    data = File.open(@file, 'rb') {|io| io.read }
+    @data = data.split("\n")
+  end
+  
+  def format_data
+    @data.each do |data|
+      ip_data = data.split(":")
+      if @result[ip_data[0]].nil?
+        create_ip_key(data)
+      else
+        append_ip_data(data)
+      end
+    end
+  end
+
+  def get_result
+    prepare_file
+    format_data
+    @result.values
+  end
+  
+  private
+
+  def create_ip_key(data)
+    sub_address = sort_data(data.split(":")[1].split(","))
+    @result[data.split(":")[0]] = data.split(":")[0] + ":" + sub_address
+  end
+  
+  def append_ip_data(data)
+    new_sub_address = data.split(":")[1]
+    old_sub_address = @result[data.split(":")[0]].split(":")[1]
+    append_sub_address = split_and_compare_data(old_sub_address, new_sub_address)
+    sorted_sub_address = sort_data(append_sub_address)
+    new_address = data.split(":")[0] + ":" + sorted_sub_address
+    @result[data.split(":")[0]] = new_address
+  end
+
+  def sort_data(data)
+    data.map(&:to_i).sort.join(",")
+  end
+
+  def split_and_compare_data(old_sub_address, new_sub_address)
+    (old_sub_address.split(",") | new_sub_address.split(","))
+  end
+end
+
+p "IP FORMATTER"
+p ""
+formatter = IpFormatter.new(ARGV[0])
+p formatter.get_result
+p ""
